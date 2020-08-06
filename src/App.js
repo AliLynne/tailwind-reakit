aa0d7b0b07633d343e59d20f86b8c56a2239740d
AliLynne <alilynne.thompson@gmail.com>
2020-08-06 10:26:41 -0700

    create Header and Footer components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { Button } from 'reakit';
+import { Button, Box, useToolbarState } from 'reakit';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
+  const toolbar = useToolbarState({ loop: true });
+
   return (
-    <div className="container bg-purple-400 h-screen flex flex-col">
+    <div className="container h-screen flex flex-col">
       <a id="skip-nav" href="#main-content" className="sr-only">
         Skip to Content
       </a>
-      <header>
-        <h1>Page Title</h1>
-        <nav>
-          <Button onClick={() => console.log('click!')}>
-            Nav Button
-          </Button>
-        </nav>
-      </header>
-      <main id="main-content" className="flex-grow">
+      <Header title="My Title" toolbar={toolbar} />
+      <Box as="main" id="main-content" className="flex-grow p-3">
         <p>Main Content Should Go Here</p>
-      </main>
-      <footer>
-        <p>Footer Content Should Go Here</p>
-      </footer>
+      </Box>
+      <Footer />
     </div>
   );
 }
